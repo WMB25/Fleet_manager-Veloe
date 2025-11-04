bee2211f6a64db7d41a324245f2588f810ba6a26
Walison M.B <Walisonmourabraz@gmail.com>
2025-11-04 17:48:16 -0300

    chore: update funções

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,8 +4,8 @@ INSERT INTO customers (id, Name, Document, email, phone) VALUES
 (3, 'Mario Silva', '76544231212', 'mariosilva@outlook.com', '(11)974323221');
 
 INSERT INTO vehicles (id, Brand, Model, Plate, type, vehicle_owner) VALUES
-(1, 'Toyota', 'Corolla', 'ABC1234', 'CARRO', 1),
-(2, 'Honda', 'Civic', 'DEF5678', 'CARRO', 1),
-(3, 'Ford', 'Ranger', 'GHI9012', 'CAMINHAO', 2),
-(4, 'Volkswagen', 'Gol', 'JKL3456', 'CARRO', 3),
-(5, 'Yamaha', 'MT-07', 'MNO7890', 'MOTO', 2);
+(1, 'Toyota', 'Corolla', 'ABC1234', 'CAR', 1),
+(2, 'Honda', 'Civic', 'DEF5678', 'CAR', 1),
+(3, 'Ford', 'Ranger', 'GHI9012', 'CAR', 2),
+(4, 'Volkswagen', 'Gol', 'JKL3456', 'CAR', 3),
+(5, 'Yamaha', 'MT-07', 'MNO7890', 'MOTOCICLE', 2);
