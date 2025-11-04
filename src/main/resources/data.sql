1351a40f03ae8d3709d1bda1b26ecd9417e20f47
Walison M.B <Walisonmourabraz@gmail.com>
2025-11-04 16:37:53 -0300

    Feat: adicionando APIs e config de data.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO customers (id, name, document, email, phone) VALUES
+(1, 'joão Silva', '13684603281', 'joaosilva@gmail.com', '(11)95672411'),
+(2, 'Mario toledo', '12454378905', 'mariotoledo@hotmail.com', '(11)963214512'),
+(3, 'Mario Silva', '76544231212', 'mariosilva@outlook.com', '(11)974323221');
+
+INSERT INTO vehicles (id, brand, model, license_plate, type, owner_id) VALUES
+(1, 'Toyota', 'Corolla', 'ABC1234', 'CARRO', 1),
+(2, 'Honda', 'Civic', 'DEF5678', 'CARRO', 1),
+(3, 'Ford', 'Ranger', 'GHI9012', 'CAMINHAO', 2),
+(4, 'Volkswagen', 'Gol', 'JKL3456', 'CARRO', 3),
+(5, 'Yamaha', 'MT-07', 'MNO7890', 'MOTO', 2);
